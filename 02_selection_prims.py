cb0f6d32dd84284cda3ffe0b827afeafd45ac0df
Parth Barse <parthbarse72@gmail.com>
2023-11-05 03:29:27 +0530

    update

--- 02_selection_prims.py
+++ 02_selection_prims.py
@@ -7,103 +7,56 @@ def selection_sort(n):
         n[i], n[min_ind] = n[min_ind], n[i]
     return n
 
+
 n = [34, 45, 12, 56, 1, 8]
 sorted_array = selection_sort(n)
 print(sorted_array)
 
-def prim_mst(graph):
-    V = len(graph)  
-    parent = [-1] * V
-    key = [float('inf')] * V
-    key[0] = 0  
-    visited = [False] * V  
+# graph = [
+#     [0, 4, 3, 0, 5],
+#     [4, 0, 1, 7, 0],
+#     [3, 1, 0, 4, 0],
+#     [0, 7, 4, 0, 2],
+#     [5, 0, 0, 2, 0]
+# ]
+# print(graph)
 
-    print("--------------------------")
-    print("Initialization :- ")
-    print("key : ",key)
-    print("Parent : ", parent)
-    print("--------------------------\n\n")
+graph=[]
+v=int(input("no of vertices:"))
+for i in range(v):
+    neighbors = input( )
+    neighbors = neighbors.split()
+    graph.append(neighbors)
 
-    for _ in range(V):  
-        min_value = float('inf')  
-        min_index = -1
+for element in graph:
+    for j in range(v):
+        element[j]=int(element[j])
+        
+v = len(graph)
 
-        for v in range(V):
-            if key[v] < min_value and not visited[v]:
-                min_value = key[v]
-                min_index = v
+visited = [0]*v
 
-        u = min_index
-        visited[u] = True  
+no_edge = 0
+visited[0] = True
 
-        for v in range(V):
-            if graph[u][v] and not visited[v] and graph[u][v] < key[v]:
-                key[v] = graph[u][v]  
-                parent[v] = u  
+while (no_edge < v-1):
+    min = float("inf")
+    x = y = 0
+    for i in range(v):
+        if visited[i]:
+            for j in range(v):
+                if ((not visited[j] and graph[i][j])):
+                    if graph[i][j] < min:
+                        min = graph[i][j]
+                        x, y = i, j
+    print(f"{x} - {y} :{graph[x][y]}")
+    visited[y] = True
+    no_edge += 1
 
-    print("Edge \tWeight")
-    for i in range(1, V):
-        print(parent[i], "-", i, "\t", graph[parent[i]][i])
-
-graph = [
-    [0, 2, 0, 6, 0],
-    [2, 0, 3, 8, 5],
-    [0, 3, 0, 0, 7],
-    [6, 8, 0, 0, 9],
-    [0, 5, 7, 9, 0]
-]
-
-prim_mst(graph)
-
-
-# class Graph:
-#     def __init__(self, vertices):
-#         self.V = vertices
-#         self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
-
-#     def add_edge(self, u, v, weight):
-#         self.graph[u][v] = weight
-#         self.graph[v][u] = weight
-
-#     def min_key(self, key, visited):
-#         min_value = float('inf')
-#         min_index = -1
-#         for v in range(self.V):
-#             if key[v] < min_value and not visited[v]:
-#                 min_value = key[v]
-#                 min_index = v
-#         return min_index
-
-#     def prim_mst(self):
-#         key = [float('inf')] * self.V
-#         parent = [-1] * self.V
-#         key[0] = 0
-#         visited = [False] * self.V
-
-#         for _ in range(self.V):
-#             u = self.min_key(key, visited)
-#             visited[u] = True
-
-#             for v in range(self.V):
-#                 if self.graph[u][v] > 0 and not visited[v] and self.graph[u][v] < key[v]:
-#                     key[v] = self.graph[u][v]
-#                     parent[v] = u
-
-#         print("Edge\tWeight")
-#         for i in range(1, self.V):
-#             print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
-
-# # Create a graph with 5 vertices
-# g = Graph(5)
-
-# # Add different edges to the graph
-# g.add_edge(0, 1, 4)
-# g.add_edge(0, 2, 3)
-# g.add_edge(1, 2, 1)
-# g.add_edge(1, 3, 7)
-# g.add_edge(2, 3, 4)
-# g.add_edge(3, 4, 2)
-# g.add_edge(4, 0, 5)
-
-# # Find and print the MST
-# g.prim_mst()
+'''
+0  4  3  0  5
+4  0  1  7  0
+3  1  0  4  0
+0  7  4  0  2
+5  0  0  2  0
+'''
--- 04_selection_dijkstras.py
+++ 04_selection_dijkstras.py
@@ -1,37 +1,49 @@
-def dijkstra(graph, start, end):
-    dist = {node: float('inf') for node in graph}
-    prev = {node: None for node in graph}
-    dist[start] = 0
-    unvisited = set(graph)
+def selection_sort(n):
+    for i in range(len(n)):
+        min_ind = i
+        for j in range(i+1, len(n)):
+            if n[j] < n[min_ind]:
+                min_ind = j
+        n[i], n[min_ind] = n[min_ind], n[i]
+    return n
 
-    while unvisited:
-        current = min(unvisited, key=lambda node: dist[node])
-        unvisited.remove(current)
+def dijkstra(graph, source):
+    v = len(graph)
+    distance = [float("inf")] * v
+    visited = [False] * v
 
-        for neighbor, weight in graph[current].items():
-            alt = dist[current] + weight
-            if alt < dist[neighbor]:
-                dist[neighbor] = alt
-                prev[neighbor] = current
+    distance[source] = 0
 
-    path = []
-    while end:
-        path.insert(0, end)
-        end = prev[end]
+    for _ in range(v - 1):
+        min_dist = float("inf")
+        for i in range(v):
+            if not visited[i] and distance[i] < min_dist:
+                min_dist = distance[i]
+                u = i
 
-    return path
+        visited[u] = True
+
+        for j in range(v):
+            if not visited[j] and graph[u][j] and distance[u] + graph[u][j] < distance[j]:
+                distance[j] = distance[u] + graph[u][j]
+
+    return distance
 
 # Example usage:
-G = {
-    'A': {'B': 2, 'C': 5},
-    'B': {'C': 1, 'D': 7},
-    'C': {'D': 3},
-    'D': {'E': 4},
-    'E': {'A': 2}
-}
+graph = [
+            [ 0, 4, 0, 0, 0, 0, 0, 8, 0 ],
+            [ 4, 0, 8, 0, 0, 0, 0, 11, 0 ],
+            [ 0, 8, 0, 7, 0, 4, 0, 0, 2 ],
+            [ 0, 0, 7, 0, 9, 14, 0, 0, 0 ],
+            [ 0, 0, 0, 9, 0, 10, 0, 0, 0 ],
+            [ 0, 0, 4, 14, 10, 0, 2, 0, 0 ],
+            [ 0, 0, 0, 0, 0, 2, 0, 1, 6 ],
+            [ 8, 11, 0, 0, 0, 0, 1, 0, 7 ],
+            [ 0, 0, 2, 0, 0, 0, 6, 7, 0]
+]
+source = 0
 
-start = 'A'
-end = 'D'
+shortest_distances = dijkstra(graph, source)
 
-result = dijkstra(G, start, end)
-print("Shortest Path:", result)
+for i, distance in enumerate(shortest_distances):
+    print(f"Shortest distance from {source} to {i} is {distance}")
